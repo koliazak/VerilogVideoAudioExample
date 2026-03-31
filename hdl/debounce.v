d6ff98531d562290848f19df49350b9b32ee0f8e
koliazak <vrmsumy3@gmail.com>
2026-04-01 00:03:55 +0200

    Changed comment

--- hdl/debounce.v
+++ hdl/debounce.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/24/2025 12:18:24 PM
-// Design Name: 
-// Module Name: debounce
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module debounce 
     #(
